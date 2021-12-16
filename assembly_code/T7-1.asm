2372adb54a69fff041dc10d9d7ae543c6504d956
calvar122 <cesar.alvarado@iteso.mx>
2021-12-15 22:11:58 -0600

    Updating the assembly_code file
    I forgot to remplace the previous assembly code, the new one is called
     T7-1, the old one was Test_Program.

---
+++ assembly_code/T7-1.asm
@@ -0,0 +1,14 @@
+.text
+	addi $s2, $s2, 0
+	addi $s0, $s0, 1
+	addi $s1, $s1, 2
+main:
+	ori $s3,$s3, 0
+	addi $s4,$s4,1
+	beq $s2,$s3, else	
+if:
+	addi $s0, $s0, 1
+	j main
+else:
+	addi $s1, $s1, 2
+	j main
